99c714dfadc510d902ec444517bf1a23d19ce79f
Muller Esposito <mulleresposito@hotmail.com>
2021-06-04 23:59:27 -0300

    Update baseURL to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const domain = process.env.NODE_ENV === 'development' 
+  ? 'http://localhost:3000'
+  : 'https://ignews-cyan.vercel.app';
+
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${domain}/api`,
 });
